70215dfcc5c229191e337ffcf2d98e25ea761d28
Mattichs <mattiabastia@gmail.com>
2025-08-04 11:20:25 +0200

    file: src/labelImage.cpp

---
+++ src/labelImage.cpp
@@ -0,0 +1,14 @@
+// this function label images 
+// INPUT: 
+// - image file path
+// - json file path
+// OUTPUT: 
+// -labeled image file path
+
+#include <string>
+
+int main() {
+    // Example input paths
+
+    return 0;
+}
