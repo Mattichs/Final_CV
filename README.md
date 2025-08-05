7a5494a4e60c672a1b5d40ac91a95041fdbbf6dd
Mattichs <mattiabastia@gmail.com>
2025-08-05 11:35:47 +0200

    ritagli di ogni oggetto su un vettore

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 We downloaded images from https://datasetninja.com/deep-nir-fruit to train our model (IDK how we will do this).
 
+We used library to read json files .....
 
 
 Time of working:
--- src/labelImage.cpp
+++ src/labelImage.cpp
@@ -9,22 +9,33 @@
 #include <filesystem>
 #include <iostream>
 #include <fstream>
-#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/features2d.hpp>
+#include <opencv2/ml.hpp>
 
 #include "json.hpp"
 
 using json = nlohmann::json;
+
+
+using namespace cv;
+using namespace cv::ml;
+
 
 int main() {
     // Example input paths
     std::string imageFilePath = "train/apple/img";
     std::string jsonFilePath = "train/apple/label";
 
+    std::vector<cv::Mat> all_crops;
+
     try {
         for(const auto& entry : std::filesystem::directory_iterator(imageFilePath)) {
             if (entry.is_regular_file()) {
                 // read image
-                cv::Mat image = imread(entry.path().string(), cv::IMREAD_GRAYSCALE);
+                cv::Mat image = cv::imread(entry.path().string());
                 if(image.empty()) {
                     std::cerr << "Could not read the image: " << entry.path() << std::endl;
                     continue;
@@ -43,29 +54,38 @@ int main() {
                 file >> j;
                 // leggere numero elementi dell'attributo objects e trovare i quadrati e stamparli sull'immagine
                 // ho angolo in alto a sinistra e angolo in basso a destra
+                                
                 for(const auto& object : j["objects"]) {
                     int x1 = object["points"]["exterior"][0][0];
                     int y1 = object["points"]["exterior"][0][1];
                     int x2 = object["points"]["exterior"][1][0];
                     int y2 = object["points"]["exterior"][1][1];
-                    //std::string classTitle = object["classTitle"];
-                    cv::rectangle(image, cv::Point(x1, y1), cv::Point(x2, y2), cv::Scalar(255, 0, 0), 2);
+                    
+                    //cv::rectangle(image, cv::Point(x1, y1), cv::Point(x2, y2), cv::Scalar(255, 0, 0), 2);
+                    cv::Rect roi(cv::Point(x1, y1), cv::Point(x2, y2));
+                    roi &= cv::Rect(0, 0, image.cols, image.rows);
+
+                    if (roi.width > 0 && roi.height > 0) {
+                        cv::Mat crop = image(roi).clone(); // clone per copia indipendente
+                        all_crops.push_back(crop);
+                    }
+                
                 }
-                std::cout << "Reading JSON from: " << filename << std::endl;
-                cv::imshow("Image", image);
-                cv::waitKey(0);
+                //std::cout << "Reading JSON from: " << filename << std::endl;
+                //cv::imshow("Image", image);
+                //cv::waitKey(0);
             }
-            /* if (entry.is_regular_file()) {
-                std::ifstream file(entry.path());
-                json j;
-                file >> j;
-                std::cout << "File: " << entry.path().string() << std::endl;
-                std::cout << "Objects: " << j["objects"][0]["classTitle"] << std::endl;
-            } */
         }
+        std::cout << "Numero totale di ritagli: " << all_crops.size() << std::endl;
+        // Mostra i ritagli 
+        /* for (size_t i = 0; i < all_crops.size(); ++i) {
+            cv::imshow("Crop " + std::to_string(i), all_crops[i]);
+            cv::waitKey(0); // mostra ogni 0.5 secondi
+        } */
     } catch(const std::filesystem::filesystem_error& e) {
         std::cerr << "Error reading: " << e.what() << std::endl;
     }
+
 
     return 0;
 }
