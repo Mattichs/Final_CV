ec3da7592c75ff883e3ae4d81a1d6433411a7842
cognoxd <cognac14265@gmail.com>
2025-08-12 08:55:12 +0200

    cascade riaddestrato e aggiunto cmake

---
+++ build/CMakeFiles/mylib.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mylib.dir/src/detect.cpp.o"
+  "CMakeFiles/mylib.dir/src/detect.cpp.o.d"
+  "CMakeFiles/mylib.dir/src/labelImage.cpp.o"
+  "CMakeFiles/mylib.dir/src/labelImage.cpp.o.d"
+  "CMakeFiles/mylib.dir/src/main.cpp.o"
+  "CMakeFiles/mylib.dir/src/main.cpp.o.d"
+  "libmylib.a"
+  "libmylib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mylib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
